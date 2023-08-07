7f1444d07e9470f36d6b0f22b3245a73bd00035a
tivt <tivt231@gmail.com>
2023-08-07 17:27:45 -0300

    feat: added error-catcher to api-router

--- src/HTTP/api-router.ts
+++ src/HTTP/api-router.ts
@@ -1,4 +1,4 @@
-import { Router } from 'express';
+import { Request, Response, Router } from 'express';
 import { get_auth_access_controller } from '../features/auth/auth-access/index.singletons';
 
 export const api_router = Router();
@@ -10,4 +10,9 @@ api_router.use(async (req, res, next) => {
 api_router.get('/', (req, res) => {
   res.status(200);
   res.json({ message: 'Hello from api' });
+});
+
+api_router.use((error: Error, req: Request, res: Response) => {
+  res.status(500);
+  res.json({ message: error.message });
 });
