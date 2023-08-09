7c44248e2b34740fbe0f4be20b8e22daf10ef2c2
tivt <tivt231@gmail.com>
2023-08-09 14:58:42 -0300

    feat: implemented task sub-features in task-router

--- src/HTTP/task-router.ts
+++ src/HTTP/task-router.ts
@@ -1,4 +1,8 @@
 import { Request, Response, Router } from 'express';
+import { get_create_task_controller } from '../features/task/create-task/index.singletons';
+import { get_get_tasks_controller } from '../features/task/get-tasks/index.singletons';
+import { get_edit_task_controller } from '../features/task/edit-task/index.singletons';
+import { get_delete_task_controller } from '../features/task/delete-task/index.singletons';
 
 export const task_router = Router();
 
@@ -7,13 +11,21 @@ task_router.get('/', (req, res) => {
   res.json({ message: 'Hello from task' });
 });
 
-task_router.post('/', async (req, res) => {});
+task_router.post('/', async (req, res) => {
+  await get_create_task_controller().control(req, res);
+});
 
-task_router.get('/', async (req, res) => {});
+task_router.get('/', async (req, res) => {
+  await get_get_tasks_controller().control(req, res);
+});
 
-task_router.put('/:task_id', async (req, res) => {});
+task_router.put('/:task_id', async (req, res) => {
+  await get_edit_task_controller().control(req, res);
+});
 
-task_router.delete('/:task_id', async (req, res) => {});
+task_router.delete('/:task_id', async (req, res) => {
+  await get_delete_task_controller().control(req, res);
+});
 
 task_router.use((error: Error, req: Request, res: Response) => {
   res.status(500);
